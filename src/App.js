6a53e41440c40c374bc98e7d21bc09e25b4efc74
miguelmassv <miguelmassv@hotmail.com>
2019-02-25 14:12:01 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      value: ""
+    };
+
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.counter.bind(this)} value={this.state.value}></textarea>
+        <div className="counter">{this.state.value.length}</div>
       </div>
     );
   }
+
+counter(event){
+  this.setState({
+    value: event.target.value
+  })
+};
 }
 
 export default App;
